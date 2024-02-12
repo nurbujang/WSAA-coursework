63518d47919b89a475eea27dfb34742f573a2335
rsalgo <tinisufina@gmail.com>
2024-02-12 19:20:04 +0000

    currentweather.py and README

---
+++ assignments/currentweather.py
@@ -0,0 +1,41 @@
+'''
+Assignment for Web Services and Applications (Spring 2024)
+currentweather.py
+Author: Nur Bujang
+
+Using the URL below:
+https://api.open-meteo.com/v1/forecast?latitude=53.82&longitude=-9.5&current=temperature_2m
+
+Write a python program called currentweather.py that will print out the current temperature on the console (and only the temperature).
+I have set the lat/long to my location, you may use that or a different location.
+
+Last few marks:
+Look at the documentation (below) and print out the current wind direction (10m) as well.
+
+🌤️ Free Open-Source Weather API | Open-Meteo.com (https://open-meteo.com/)
+
+Save the file into your assignments directory (or you can make a separate repo for it, I do not mind).
+Copy a link to the directory/repo to here (please only put a link in here, other information can be in the readme)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+REFERENCES
+
+
+_______________________________________________________________________________________________
+End
+'''
