3dc5abe46904a55db6cea7be1199935e7a0e4cc2
rsalgo <tinisufina@gmail.com>
2024-02-24 23:02:08 +0000

    Lecture 5.1, problem with from config import apikeys

--- labs/my5htmltopdfdemo.py
+++ labs/my5htmltopdfdemo.py
@@ -2,6 +2,30 @@
 # This is to demonstrate API Keys
 # Author: Andrew Beatty
 
+# import requests
+# import urllib.parse
+
+# targetUrl = "https://en.wikipedia.org"
+# #targetUrl = "https://www.atu.ie/"
+
+# apikey = "ljWKVtWMmaQaPkq8wJZ3Es22W2MRnf2c9R5fK8QE7f5cjiJgDW6GJ6AQk7yh7J6k"
+# apiurl = 'https://api.html2pdf.app/v1/generate'
+
+# params = {'html': targetUrl,'apiKey': apikey}
+# parsedparams = urllib.parse.urlencode(params)
+
+# requestUrl = apiurl +"?" + parsedparams 
+# print (requestUrl)  # just for debugging
+
+# response = requests.get(requestUrl)
+
+# print (response.status_code)
+# result =response.content
+
+# with open("my5document.pdf", "wb") as handler:
+#     handler.write(result)
+
+# my5document.pdf, my5config,py, then below:
 
 import requests
 import urllib.parse
@@ -17,12 +41,37 @@ params = {'html': targetUrl,'apiKey': apikey}
 parsedparams = urllib.parse.urlencode(params)
 
 requestUrl = apiurl +"?" + parsedparams 
-print (requestUrl)
+print (requestUrl)  # just for debugging
 
 response = requests.get(requestUrl)
 
 print (response.status_code)
 result =response.content
 
-with open("document.pdf", "wb") as handler:
+with open("my5document.pdf", "wb") as handler:
     handler.write(result)
+
+
+# import requests
+# import urllib.parse
+# from config import apikeys as cfg
+
+# targetUrl = "https://en.wikipedia.org"
+# #targetUrl = "https://www.atu.ie/"
+
+# apikey = cfg["htmltopdfkey"]
+# apiurl = 'https://api.html2pdf.app/v1/generate'
+
+# params = {'html': targetUrl,'apiKey': apikey}
+# parsedparams = urllib.parse.urlencode(params)
+
+# requestUrl = apiurl +"?" + parsedparams 
+# print (requestUrl)
+
+# response = requests.get(requestUrl)
+
+# print (response.status_code)
+# result =response.content
+
+# with open("document.pdf", "wb") as handler:
+#     handler.write(result)
