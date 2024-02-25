8390515d199d7fe8c2c2a4c3f2157e2ac75a4d08
rsalgo <tinisufina@gmail.com>
2024-02-25 21:14:22 +0000

    lab 5.3

--- labs/mylab05.03.01-githubbymodule.py
+++ labs/mylab05.03.01-githubbymodule.py
@@ -1,28 +1,75 @@
-# to use this install package
-# pip install PyGithub
+# to use this install package : pip install PyGithub
+
 from github import Github
 import requests
 from config import config as cfg
 
-apikey = cfg["githubkey"]
+apikey = cfg["githubkey"] # use own key
 
 g = Github(apikey)
 
-#for repo in g.get_user().get_repos():
-#    print(repo.name)
-    #repo.edit(has_wiki=False)
-    # to see all the available attributes and methods
-    #print(dir(repo))
-# make sure this replository exists, and that the path is correct
-repo = g.get_repo("andrewbeattycourseware/aprivateone")
+for repo in g.get_user().get_repos():
+   print(repo.name)
+#     #repo.edit(has_wiki=False)
+#     # to see all the available attributes and methods
+#     #print(dir(repo))
+# # make sure this repository exists, and that the path is correct
+repo = g.get_repo("nurbujang/private")
+
 #print(repo.clone_url)
 fileInfo = repo.get_contents("test.txt")
 urlOfFile = fileInfo.download_url
-#print (urlOfFile)
+print (urlOfFile)
 response = requests.get(urlOfFile)
 contentOfFile = response.text
-#print (contentOfFile)
+print (contentOfFile)
 newContents = contentOfFile + " more stuff 2 \n"
 print (newContents)
+
+# update_file(path, message, content, sha, branch=NotSet, committer=NotSet, author=NotSet)
 gitHubResponse=repo.update_file(fileInfo.path,"updated by prog",newContents,fileInfo.sha)
 print (gitHubResponse)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # to use this install package
+# # pip install PyGithub
+# from github import Github
+# import requests
+# from config import config as cfg
+
+# apikey = cfg["githubkey"]
+
+# g = Github(apikey)
+
+# #for repo in g.get_user().get_repos():
+# #    print(repo.name)
+#     #repo.edit(has_wiki=False)
+#     # to see all the available attributes and methods
+#     #print(dir(repo))
+# # make sure this replository exists, and that the path is correct
+# repo = g.get_repo("andrewbeattycourseware/aprivateone")
+# #print(repo.clone_url)
+# fileInfo = repo.get_contents("test.txt")
+# urlOfFile = fileInfo.download_url
+# #print (urlOfFile)
+# response = requests.get(urlOfFile)
+# contentOfFile = response.text
+# #print (contentOfFile)
+# newContents = contentOfFile + " more stuff 2 \n"
+# print (newContents)
+# gitHubResponse=repo.update_file(fileInfo.path,"updated by prog",newContents,fileInfo.sha)
+# print (gitHubResponse)
