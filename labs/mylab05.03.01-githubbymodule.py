9237283b66e0f100abdee246458edeac57bd4e99
rsalgo <tinisufina@gmail.com>
2024-02-26 08:00:33 +0000

    text.txt file

--- labs/mylab05.03.01-githubbymodule.py
+++ labs/mylab05.03.01-githubbymodule.py
@@ -8,8 +8,8 @@ apikey = cfg["githubkey"] # use own key
 
 g = Github(apikey)
 
-for repo in g.get_user().get_repos():
-   print(repo.name)
+# for repo in g.get_user().get_repos():
+#    print(repo.name)
 #     #repo.edit(has_wiki=False)
 #     # to see all the available attributes and methods
 #     #print(dir(repo))
